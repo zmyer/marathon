02a04547bb6ff3aae55b71b8a0240fcd591c4978
Johannes Unterstein <junterstein@mesosphere.io>
2016-09-08 21:23:12 +0200

    maagic...

--- src/test/scala/mesosphere/marathon/MarathonSpec.scala
+++ src/test/scala/mesosphere/marathon/MarathonSpec.scala
@@ -1,6 +1,12 @@
 package mesosphere.marathon
 
+import mesosphere.marathon.core.instance.Instance
+import mesosphere.marathon.core.task.Task
 import org.scalatest.{ BeforeAndAfter, FunSuiteLike, OptionValues }
 import org.scalatest.mockito.MockitoSugar
 
-trait MarathonSpec extends FunSuiteLike with BeforeAndAfter with MockitoSugar with OptionValues
+trait MarathonSpec extends FunSuiteLike with BeforeAndAfter with MockitoSugar with OptionValues {
+  implicit def tasksToInstances(tasks: Iterable[Task]): Iterable[Instance] = tasks.map(task => Instance(task))
+
+  implicit def taskToInstance(task: Task): Instance = Instance(task)
+}
