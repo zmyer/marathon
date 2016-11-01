30c94164a2a7eb8363164a36748c3f0ceef5ca4b
Ken Sipe <kensipe@gmail.com>
2016-11-01 14:47:39 -0500

    reorganized tests

--- tests/system/test_marathon_basics.py
+++ tests/system/test_marathon_basics.py
@@ -391,6 +391,88 @@ def test_health_failed_check():
         assert new_tasks[0]['id'] != tasks[0]['id']
 
 
+def test_pinned_task_scales_on_host_only():
+    app_def = app('pinned')
+    host = ip_other_than_mom()
+    pin_to_host(app_def, host)
+
+    with marathon_on_marathon():
+        client = marathon.create_client()
+        client.add_app(app_def)
+        deployment_wait()
+
+        tasks = client.get_tasks('/pinned')
+        assert len(tasks) == 1
+        assert tasks[0]['host'] == host
+
+        client.scale_app('pinned', 10)
+        deployment_wait()
+
+        tasks = client.get_tasks('/pinned')
+        assert len(tasks) == 10
+        for task in tasks:
+            assert task['host'] == host
+
+
+def test_pinned_task_recovers_on_host():
+    app_def = app('pinned')
+    host = ip_other_than_mom()
+    pin_to_host(app_def, host)
+
+    with marathon_on_marathon():
+        client = marathon.create_client()
+        client.add_app(app_def)
+        deployment_wait()
+        tasks = client.get_tasks('/pinned')
+
+        kill_process_on_host(host, '[s]leep')
+        deployment_wait()
+        new_tasks = client.get_tasks('/pinned')
+
+        assert tasks[0]['id'] != new_tasks[0]['id']
+        assert new_tasks[0]['host'] == host
+
+
+def test_pinned_task_does_not_scale_to_unpinned_host():
+    app_def = app('pinned')
+    host = ip_other_than_mom()
+    pin_to_host(app_def, host)
+    # only 1 can fit on the node
+    app_def['cpus'] = 3.5
+    with marathon_on_marathon():
+        client = marathon.create_client()
+        client.add_app(app_def)
+        deployment_wait()
+        tasks = client.get_tasks('/pinned')
+        client.scale_app('pinned', 2)
+        # typical deployments are sub 3 secs
+        time.sleep(5)
+        deployments = client.get_deployments()
+        tasks = client.get_tasks('/pinned')
+
+        assert len(deployments) == 1
+        assert len(tasks) == 1
+
+
+def test_pinned_task_does_not_find_unknown_host():
+    app_def = app('pinned')
+    host = ip_other_than_mom()
+    pin_to_host(app_def, '10.255.255.254')
+    # only 1 can fit on the node
+    app_def['cpus'] = 3.5
+    with marathon_on_marathon():
+        client = marathon.create_client()
+        client.add_app(app_def)
+        # deploys are within secs
+        # assuming after 10 no tasks meets criteria
+        time.sleep(10)
+
+        tasks = client.get_tasks('/pinned')
+        assert len(tasks) == 0
+
+        client.remove_app(app_def['id'])
+
+
 def setup_function(function):
     with marathon_on_marathon():
         delete_all_apps_wait()
