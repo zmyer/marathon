c59cbeab8cca9f92c813a663782a309842659089
Jason Gilanfarr <jgilanfarr@mesosphere.com>
2016-11-09 12:14:50 -0800

    Introduce lightweight WorkQueue and KeyedLock
    Summary:
    - CapConcurrency is very heavyweight and not easily layered.
    - LockManager is very heavyweight and ends up consuming a thread
      anytime something needs to block.
      This ends up creating _tons_ of extra threads that are just waiting
      on a semaphore.
    - Bring out a very simple "WorkQueue" and then layer a "KeyedLock" on it
      that provides all of this behavior in a non-blocking manner.
    
    A non-trivial amount of time is spent in CapConcurrency and LockManager when looking at performance data.
    In addition, creating a group with a TON of apps will create thousands of threads and will likely bring
    down the JVM as a result. This should avoid creating a large percentage of the threads.
    
    Test Plan: sbt test
    
    Reviewers: jdef, aquamatthias, mpark
    
    Subscribers: marathon-team
    
    JIRA Issues: DCOS-9121, MARATHON-1147, MARATHON-1108
    
    Differential Revision: https://phabricator.mesosphere.com/D171

--- src/main/scala/mesosphere/marathon/util/WorkQueue.scala
+++ src/main/scala/mesosphere/marathon/util/WorkQueue.scala
@@ -9,7 +9,30 @@ import mesosphere.marathon.functional._
 
 import scala.collection.mutable
 
+/**
+  * Allows capping the maximum number of concurrent tasks in an easy manner:
+  * {{{
+  *   val queue = WorkQueue("zk-access", 32, 1000)
+  *   queue {
+  *     // some future
+  *   }
+  *   queue.blocking {
+  *     // some blocking method
+  *   }
+  *   ...
+  * }}}
+  *
+  * @param name The name of the queue
+  * @param maxConcurrent The maximum number of work items allowed in parallel.
+  * @param maxQueueLength The maximum number of items allowed to queue up, if the length is exceeded,
+  *                       the future will fail with an IllegalStateException
+  * @param parent An optional parent queue (this allows stacking). This can come in handy with [[KeyedLock]]
+  *               so that you can say "lock on this key" and "limit the total number of outstanding to 32".
+  *               There can be other use cases as well, where you may want to limit the number of
+  *               total operations for a class to X while limiting a particular operation to Y where Y < X.
+  */
 case class WorkQueue(name: String, maxConcurrent: Int, maxQueueLength: Int, parent: Option[WorkQueue] = None) {
+  require(maxConcurrent > 0 && maxQueueLength >= 0)
   private case class WorkItem[T](f: () => Future[T], ctx: ExecutionContext, promise: Promise[T])
   private val queue = new ConcurrentLinkedQueue[WorkItem[_]]()
   private val totalOutstanding = new AtomicInteger(0)
@@ -35,23 +58,39 @@ case class WorkQueue(name: String, maxConcurrent: Int, maxQueueLength: Int, pare
     } { run(_) }
   }
 
+  def blocking[T](f: => T)(implicit ctx: ExecutionContext): Future[T] = {
+    apply(Future(concurrent.blocking(f)))
+  }
+
   def apply[T](f: => Future[T])(implicit ctx: ExecutionContext): Future[T] = {
-    val previouslyOutstanding = totalOutstanding.getAndUpdate((total: Int) => if (total <= maxConcurrent) total + 1 else total)
+    val previouslyOutstanding = totalOutstanding.getAndUpdate((total: Int) => if (total < maxConcurrent) total + 1 else total)
     if (previouslyOutstanding < maxConcurrent) {
       val promise = Promise[T]()
       run(WorkItem(() => f, ctx, promise))
     } else {
-      val promise = Promise[T]()
-      queue.add(WorkItem(() => f, ctx, promise))
-      promise.future
+      if (queue.size + 1 > maxQueueLength) {
+        Future.failed(new IllegalStateException(s"$name queue may not exceed $maxQueueLength entries"))
+      } else {
+        val promise = Promise[T]()
+        queue.add(WorkItem(() => f, ctx, promise))
+        promise.future
+      }
     }
   }
 }
 
-case class KeyedLock[T](name: String, maxQueueLength: Int, parent: Option[WorkQueue] = None) {
-  private val queues = Lock(mutable.HashMap.empty[T, WorkQueue].withDefault(hash => WorkQueue(s"$name-$hash", maxConcurrent = 1, maxQueueLength, parent)))
+/**
+  * Allows serialized execution of futures based on a Key (specifically the Hash of that key).
+  * Does not block any threads and will schedule any items on a parent queue, if supplied.
+  */
+case class KeyedLock[K](name: String, maxQueueLength: Int, parent: Option[WorkQueue] = None) {
+  private val queues = Lock(mutable.HashMap.empty[K, WorkQueue])
 
-  def apply(key: T)(f: => Future[T])(implicit ctx: ExecutionContext): Future[T] = {
-    queues(_(key)(f))
+  def blocking[T](key: K)(f: => T)(implicit ctx: ExecutionContext): Future[T] = {
+    apply(key)(Future(concurrent.blocking(f)))
+  }
+
+  def apply[T](key: K)(f: => Future[T])(implicit ctx: ExecutionContext): Future[T] = {
+    queues(_.getOrElseUpdate(key, WorkQueue(s"$name-$key", maxConcurrent = 1, maxQueueLength, parent))(f))
   }
 }
--- src/test/scala/mesosphere/marathon/util/WorkQueueTest.scala
+++ src/test/scala/mesosphere/marathon/util/WorkQueueTest.scala
@@ -6,11 +6,11 @@ import java.util.concurrent.atomic.AtomicInteger
 
 import mesosphere.UnitTest
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.duration._
 
 class WorkQueueTest extends UnitTest {
-
   "WorkQueue" should {
     "cap the maximum number of concurrent operations" in {
       val counter = new AtomicInteger(0)
@@ -20,19 +20,15 @@ class WorkQueueTest extends UnitTest {
       val exited2 = new Semaphore(0)
 
       val queue = WorkQueue("test", maxConcurrent = 1, maxQueueLength = Int.MaxValue)
-      queue {
-        Future {
-          waitToExit1.acquire()
-          exited1.release()
-        }
+      queue.blocking {
+        waitToExit1.acquire()
+        exited1.release()
       }
 
-      queue {
-        Future {
-          counter.incrementAndGet()
-          waitToExit2.acquire()
-          exited2.release()
-        }
+      queue.blocking {
+        counter.incrementAndGet()
+        waitToExit2.acquire()
+        exited2.release()
       }
 
       waitToExit1.release()
@@ -42,6 +38,68 @@ class WorkQueueTest extends UnitTest {
       waitToExit2.release()
       exited2.acquire()
       counter.get() should equal(1)
+    }
+    "complete the future with a failure if the queue is capped" in {
+      val queue = WorkQueue("abc", maxConcurrent = 1, maxQueueLength = 0)
+      val semaphore = new Semaphore(0)
+      queue.blocking {
+        semaphore.acquire()
+      }
+
+      intercept[IllegalStateException] {
+        throw queue.blocking {
+          semaphore.acquire()
+        }.failed.futureValue
+      }
+
+    }
+    "continue executing even when the previous job failed" in {
+      val queue = WorkQueue("failures", 1, Int.MaxValue)
+      queue.blocking {
+        throw new Exception("Expected")
+      }.failed.futureValue.getMessage should equal("Expected")
+      queue.blocking {
+        7
+      }.futureValue should be(7)
+    }
+    "defer to the parent queue when defined" in {
+      val parent = new WorkQueue("parent", 1, 1) {
+        override def apply[T](f: => Future[T])(implicit ctx: ExecutionContext): Future[T] =
+          Future.successful[T](1.asInstanceOf[T])
+      }
+      val queue = WorkQueue("child", 1, 1, Some(parent))
+      queue.blocking {
+        7
+      }.futureValue should equal(1)
+    }
+  }
+  "KeyedLock" should {
+    "allow exactly one work item per key" in {
+      val lock = KeyedLock[String]("abc", Int.MaxValue)
+      val sem = new Semaphore(0)
+      val counter = new AtomicInteger(0)
+      lock.blocking("1") {
+        sem.acquire()
+      }
+      val blocked = lock.blocking("1") {
+        counter.incrementAndGet()
+      }
+      counter.get() should equal(0)
+      blocked.isReadyWithin(1.millis) should be(false)
+      sem.release()
+      blocked.futureValue should be(1)
+      counter.get() should equal(1)
+    }
+    "allow two work items on different keys" in {
+      val lock = KeyedLock[String]("abc", Int.MaxValue)
+      val sem = new Semaphore(0)
+      lock.blocking("1") {
+        sem.acquire()
+      }
+      lock.blocking("2") {
+        "done"
+      }.futureValue should equal("done")
+      sem.release()
     }
   }
 }
