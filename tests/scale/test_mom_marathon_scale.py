b2465bb0e336ac0bad675b7c50198da678c3ea8e
Ken Sipe <kensipe@gmail.com>
2016-12-19 09:33:20 -0600

    moving to new scale test approach

--- tests/scale/test_mom_marathon_scale.py
+++ tests/scale/test_mom_marathon_scale.py
@@ -1,163 +1,221 @@
-import requests
-import json
-import time
-import subprocess
-
-from shakedown import *
-from dcos import config
-from six.moves import urllib
-from common import *
 from utils import *
-
-instances_results = []
-count_results = []
-group_results = []
-
-
+from common import *
+import time
+import sys
+import os
 """
-Scale tests MoM marathons on DCOS.  It is expected that
-the preferred Marathon is installed before execution and
-that the name is 'marathon-user'.
+    assumptions:
+        1) written in progressively higher scale
+        2) mom1 and mom2 are group such that we are not switching each test
+        3) the tests are run in the order they are written
 """
+default_moms = {
+    'mom1': '1.3.6',
+    'mom2': '1.4.0-RC3'
+}
+# to be discovered
+marathons = {}
+type_test_failed = {}
+
+test_log = []
+##############
+# Test Section
+##############
+# MOM1
 
 
-def setup_function(function):
-    wait_for_service_endpoint('marathon-user')
+def test_mom1_apps_instances_1_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_instances_1_10():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_instances_1_100():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_instances_1_500():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_instances_1_1000():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_instances_1_5000():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_instances_1_10000():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_instances_1_25000():
+    run_test(sys._getframe().f_code.co_name)
+
+# def test_mom1_apps_instances_1_50000():
+#     run_test(sys._getframe().f_code.co_name)
+
+
+# counts
+def test_mom1_apps_count_1_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_count_10_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_count_100_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_count_500_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_count_1000_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom1_apps_count_5000_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+# MOM2
+def test_mom2_apps_instances_1_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_instances_1_10():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_instances_1_100():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_instances_1_500():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_instances_1_1000():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_instances_1_5000():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_instances_1_10000():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_instances_1_25000():
+    run_test(sys._getframe().f_code.co_name)
+
+
+# def test_mom2_apps_instances_1_50000():
+#     run_test(sys._getframe().f_code.co_name)
+# counts
+def test_mom2_apps_count_1_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_count_10_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_count_100_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_count_500_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_count_1000_1():
+    run_test(sys._getframe().f_code.co_name)
+
+
+def test_mom2_apps_count_5000_1():
+    run_test(sys._getframe().f_code.co_name)
+##############
+# End Test Section
+##############
+
+
+def run_test(name):
+    current_test = start_test(name, marathons)
+    test_log.append(current_test)
+    need = scaletest_resources(current_test)
+    # TODO: why marathon stops at 80%
+    if need > (available_resources() * 0.8):
+        current_test.skip('insufficient resources')
+        return
+    if previous_style_test_failed(current_test):
+        current_test.skip('smaller scale failed')
+        return
+
+    assert ensure_test_mom(current_test)
+    # wait for max
+    # respond to timeouts
     with marathon_on_marathon():
-        delete_all_apps_wait()
+        time = scale_test_apps(current_test)
+
+    if "failed" in current_test.status:
+        type_test_failed[get_mom_style_key(current_test)] = True
 
 
-def test_apps_instances_1():
-    with marathon_on_marathon():
-        time = scale_apps(1, 1)
-        instances_results.append(time)
+def get_mom_style_key(current_test):
+    return '{}_{}'.format(current_test.mom, current_test.style)
 
 
-def test_apps_instances_10():
-    with marathon_on_marathon():
-        time = scale_apps(1, 10)
-        instances_results.append(time)
+def previous_style_test_failed(test_obj):
+    failed = False
+    try:
+        failed = type_test_failed.get(get_mom_style_key(current_test))
+    except:
+        failed = False
+        pass
+
+    return failed
 
 
-def test_apps_instances_100():
-    with marathon_on_marathon():
-        time = scale_apps(1, 100)
-        instances_results.append(time)
-
-
-def test_apps_instances_500():
-    with marathon_on_marathon():
-        time = scale_apps(1, 500)
-        instances_results.append(time)
-
-
-def test_apps_instances_1000():
-    with marathon_on_marathon():
-        time = scale_apps(1, 1000)
-        instances_results.append(time)
-
-def test_apps_instances_5000():
-    with marathon_on_marathon():
-        time = scale_apps(1, 5000)
-        instances_results.append(time)
-
-def test_apps_instances_10000():
-    with marathon_on_marathon():
-        time = scale_apps(1, 10000)
-        instances_results.append(time)
-
-
-# def test_apps_instances_25000():
-#     with marathon_on_marathon():
-#         time = scale_apps(1, 25000)
-#         instances_results.append(time)
-
-
-# def test_apps_instances_50000():
-#     with marathon_on_marathon():
-#         time = scale_apps(1, 50000)
-#         instances_results.append(time)
-
-
-def test_apps_count_1():
-    with marathon_on_marathon():
-        time = scale_apps(1, 1)
-        count_results.append(time)
-
-
-def test_apps_count_10():
-    with marathon_on_marathon():
-        time = scale_apps(10, 1)
-        count_results.append(time)
-
-
-def test_apps_count_100():
-    with marathon_on_marathon():
-        time = scale_apps(100, 1)
-        count_results.append(time)
-
-
-def test_apps_count_500():
-    with marathon_on_marathon():
-        time = scale_apps(500, 1)
-        count_results.append(time)
-
-
-def test_apps_count_1000():
-    with marathon_on_marathon():
-        time = scale_apps(1000, 1)
-        count_results.append(time)
-
-
-def test_apps_count_5000():
-    with marathon_on_marathon():
-        time = scale_apps(5000, 1)
-        count_results.append(time)
-
-
-def test_groups_instances_1():
-    with marathon_on_marathon():
-        time = scale_groups(1)
-        group_results.append(time)
-
-
-def test_groups_instances_10():
-    with marathon_on_marathon():
-        time = scale_groups(10)
-        group_results.append(time)
-
-
-def test_groups_instances_100():
-    with marathon_on_marathon():
-        time = scale_groups(100)
-        group_results.append(time)
-
-
-def test_groups_instances_500():
-    with marathon_on_marathon():
-        time = scale_groups(500)
-        group_results.append(time)
-
-
-def test_groups_instances_1000():
-    with marathon_on_marathon():
-        time = scale_groups(1000)
-        group_results.append(time)
-
-def test_groups_instances_5000():
-    with marathon_on_marathon():
-        time = scale_groups(5000)
-        group_results.append(time)
+def set_mom(name):
+    try:
+        marathons[name] = os.environ[name.upper()]
+    except:
+        marathons[name] = default_moms[name]
+        pass
 
 
 def setup_module(module):
-    # verify test system requirements are met (number of nodes needed)
+    set_mom('mom1')
+    set_mom('mom2')
     cluster_info()
-    agents = get_private_agents()
-    if len(agents) < 1:
-        assert False, "Incorrect Agent count"
+    print('marathons in test: {}'.format(marathons))
+
 
 def teardown_module(module):
-    print("instance test: {}".format(instances_results))
-    print("count test: {}".format(count_results))
-    print("group test: {}".format(group_results))
+    stats = {
+        'mom1_instances': [],
+        'mom1_count': [],
+        'mom2_instances': [],
+        'mom2_count': []
+    }
+
+    for stest in test_log:
+        print(stest)
+        stest.log_events()
+        stest.log_stats()
+        print('')
+        stats.get(get_mom_style_key(stest)).append(stest.deploy_time)
+
+    print(stats)
+
+
+def get_current_test():
+    return test_log[-1]
